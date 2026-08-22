2ac31b7e36948c5813a2d5c5cc7462616e32a33b
ishandas994-cloud <ishandas994@gmail.com>
2026-08-22 23:08:20 +0530

    a socket problem solved

--- day4/chatclient2.c
+++ day4/chatclient2.c
@@ -32,7 +32,7 @@ int main()
 
     client.sin_family = AF_INET;
     client.sin_port = htons(client_port);
-    client.sin_addr.s_addr = inet_addr("127.0.0.1");
+    client.sin_addr.s_addr = htonl(INADDR_ANY);  
 
     b = bind(sockfd, (struct sockaddr *)&client, sizeof(client));
 
@@ -45,8 +45,8 @@ int main()
     printf("Bind successful\n");
 
     server.sin_family = AF_INET;
-    server.sin_port = htons(server_port);
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_port = htons(8889);
+    server.sin_addr.s_addr = inet_addr("192.168.4.101");
 
     len = sizeof(server);
 
